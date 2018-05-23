27791cca38108465659a40bb40cb85c930e71338
grey0207 <grey0207@qq.com>
2018-05-24 00:21:30 +0800

    添加获取api

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const rp = require('request-promise-native')
 const fs = require("fs")
 const cheerio = require('cheerio')
 
 const app = new express()
+app.use(cors(), bodyParser.json())
 
 app.get('/', (req, res) => {
   rp.get('http://www.pokemon.jp/zukan/scripts/data/top_zukan.json')
     .then(body => {
       res.send(JSON.parse(body))
+    })
+})
+
+app.post('/api', (req, res) => {
+  console.log(req.body);
+  rp.post('http://www.pokemon.jp/api.php',{
+    form: req.body
+  })
+    .then(body => {
+      res.send(body)
     })
 })
 
