b9622f1674f24b8b9e4c746a0439dea2df75821e
grey0207 <grey0207@qq.com>
2018-06-03 20:32:29 +0800

    增加搜索功能

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,69 +1,117 @@
 import React, { Component } from 'react'
 import Card from '../../components/Card';
+import Chip from '../../components/Chip';
 import { getZukanTop } from '../../api';
+import { type, height, weight, region } from '../../data';
 import './style.css'
 
 class Home extends Component {
-  state = {
-    zukanTop: null,
-    displayNum: 12,
-    AllZukanTop: []
-  }
-  componentDidMount() {
-    getZukanTop().then(res => {
-      this.setState({
-        zukanTop: res.data.slice(0,12),
-        AllZukanTop: res.data
-      })
-    })
-  }
+    state = {
+        zukanTop: null,
+        filterZukanTop: [],
+	    AllZukanTop: [],
+        displayCount: 12,
+        pageIndex:1,
+    }
 
-  displayIncrease () {
-    let newDisplyNum = this.state.displayNum + 12
-    this.setState({ displayNum: newDisplyNum }, () => {
-      this.setState({ zukanTop: this.state.AllZukanTop.slice(0, this.state.displayNum) })
-    })
-  }
+    componentDidMount () {
+        getZukanTop().then(res => {
+            this.setState({
+                zukanTop: res.data.slice(0, this.state.displayCount * this.state.pageIndex),
+                filterZukanTop: res.data,
+                AllZukanTop: res.data
+            })
+        })
+    }
 
-  randomPokemon () {
-    const shuffle = ([...arr]) => {
-      let m = arr.length;
-      while (m) {
-        const i = Math.floor(Math.random() * m--);
-        [arr[m], arr[i]] = [arr[i], arr[m]];
-      }
-      return arr;
-    };
-    this.setState({ AllZukanTop: shuffle(this.state.AllZukanTop) }, () => {
-      this.setState({ zukanTop: this.state.AllZukanTop.slice(0, 12) })
-    })
-  }
+    __getFilterPokemon (e) {
+        let filterNumberPokemon = this.state.AllZukanTop.filter(item => {
+            return item.zukan_no.includes(e.target.value);
+        })
+        this.setState({
+            filterZukanTop: filterNumberPokemon,
+            pageIndex: 1
+        }, () => {
+            this.setState({ zukanTop: this.state.filterZukanTop.slice(0, this.state.displayCount * this.state.pageIndex) })
+        })
+    }
 
-  render() {
-    return (
-      <div className="home container">
-        <div className="dashboard">
-          dashboard
+    displayIncrease () {
+        let pageIndex = this.state.pageIndex + 1;
+        this.setState({ pageIndex: pageIndex }, () => {
+            this.setState({ zukanTop: this.state.filterZukanTop.slice(0, this.state.displayCount * this.state.pageIndex) })
+        })
+    }
+
+    randomPokemon () {
+        const shuffle = ([...arr]) => {
+            let m = arr.length;
+            while (m) {
+                const i = Math.floor(Math.random() * m--);
+                [arr[m], arr[i]] = [arr[i], arr[m]];
+            }
+            return arr;
+        };
+        this.setState({
+            filterZukanTop: shuffle(this.state.AllZukanTop),
+            pageIndex: 1
+        }, () => {
+            this.setState({ zukanTop: this.state.filterZukanTop.slice(0, this.state.displayCount * this.state.pageIndex) })
+        })
+    }
+
+    onKeyUp (e) {
+        e.keyCode === 13 && this.__getFilterPokemon(e);
+    }
+
+    render() {
+        return (
+        <div className="home container">
+            <div className="search card-list columns col-gapless">
+                <div className="column col-11">
+                    <div className="has-icon-right">
+                        <input type="text" className="form-input" placeholder="search pokemon number" onKeyUp={ (e) => this.onKeyUp(e) } />
+                        <i className="form-icon icon icon-search"></i>
+                    </div>
+                </div>
+                <div className="column col-1">
+                    <i className="toggle-dashboard icon icon-more-vert"></i>
+                </div>
+            </div>
+
+            {
+                this.state.zukanTop === null
+                ?   <div className="content"><img src={ require('../../assets/images/loader.gif') } className="img-responsive loading" alt="loading" /></div>
+                :   <div className="content">
+                        <div className="random-btn">
+                            <button className="btn btn-lg" onClick={ () => this.randomPokemon() }>Random</button>
+                        </div>
+                        {
+                            this.state.zukanTop.length === 0
+                            ?   <div className="no-match text-center">no match pokemon</div>
+                            :   <div className="card-list columns">
+                                    {
+                                        this.state.zukanTop.map(( formData, index ) => (
+                                            <div className="column col-6" key={ formData.zukan_no + '_' +  formData.sub_id }>
+                                                <Card formData={ formData } />
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                        }
+                        <div className="more-btn">
+                            {
+                                this.state.zukanTop.length === this.state.filterZukanTop.length
+                                ?   null
+                                :   <button className="btn btn-lg" onClick={ () => this.displayIncrease() }>More</button>
+                            }
+
+                        </div>
+                    </div>
+            }
         </div>
-        <div className="random-btn">
-          <button className="btn btn-lg" onClick={ () => this.randomPokemon() }>Random</button>
-        </div>
-        <div className="card-list columns">
-          { this.state.zukanTop === null
-            ? <div>Loading...</div>
-            : this.state.zukanTop.map(formData => (
-              <div className="column col-6" key={ formData.zukan_no + '_' +  formData.sub_id }>
-                <Card formData={ formData } />
-              </div>
-            ))
-          }
-        </div>
-        <div className="more-btn">
-          <button className="btn btn-lg" onClick={ () => this.displayIncrease() }>More</button>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default Home
