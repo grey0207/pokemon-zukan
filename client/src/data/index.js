2db2d086bb4c2a546332a5abda4a37ace4f37825
grey0207 <grey0207@qq.com>
2018-06-03 20:28:35 +0800

    增加筛选数据

---
+++ client/src/data/index.js
@@ -0,0 +1,14 @@
+export const region = [
+    {text:'カントー地方',value:'kanto'},
+    {text:'ジョウト地方',value:'jhoto'},
+    {text:'ホウエン地方',value:'hoenn'},
+    {text:'シンオウ地方',value:'sinnoh'},
+    {text:'イッシュ地方',value:'isshu'},
+    {text:'カロス地方',value:'kalos'},
+    {text:'アローラ地方',value:'arolla'}
+]
+
+export const type = ['ノーマル', 'ほのお', 'みず', 'くさ', 'でんき', 'こおり', 'かくとう', 'どく', 'じめん', 'ひこう', 'エスパー', 'むし', 'いわ', 'ゴースト', 'ドラゴン', 'あく', 'はがね', 'フェアリー']
+
+export const height = ['low','normal','high']
+export const weight = ['light','normal','heavy']
