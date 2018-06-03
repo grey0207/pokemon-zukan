306a3c686ceb8e142770844cf889602dea424a40
grey0207 <grey0207@qq.com>
2018-06-03 20:29:51 +0800

    调通detail接口

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import 'spectre.css';
+import '../node_modules/spectre.css/dist/spectre-icons.css';
 import Detail from './views/Detail'
 import Home from './views/Home'
 
@@ -10,7 +11,7 @@ class App extends Component {
       <HashRouter>
         <div className="App">
           <Route exact path="/" component={ Home } />
-          <Route path="/detail" component={ Detail }/>
+          <Route path="/detail/:page" component={ Detail }/>
         </div>
       </HashRouter>
     );
--- client/src/views/Detail/index.js
+++ client/src/views/Detail/index.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import { getDetailData } from '../../api';
 
 class Detail extends Component {
-  render() {
-    return (
-      <div>
-        Detail
-      </div>
-    )
-  }
+    state = {
+        detailData: null
+    }
+
+    componentDidMount() {
+        let { page } = this.props.match.params;
+        getDetailData(page)().then(res => {
+            this.setState({
+                detailData: res.data
+            })
+        })
+    }
+    render() {
+        return (
+            this.state.detailData === null
+            ?   <div className="content"><img src={ require('../../assets/images/loader.gif') } className="img-responsive loading" alt="loading" /></div>
+            :   <div>{ this.state.detailData.name }</div>
+        );
+    }
 }
 
-export default Detail
+export default Detail;
