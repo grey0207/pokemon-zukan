18f861733dc69a3fb94b1991f907edee7422a3bd
bobo <bobo.zhao@yo-ren.com>
2018-06-07 18:37:34 +0800

    增加minicard组件

---
+++ client/src/components/MiniCard/index.js
@@ -0,0 +1,37 @@
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+import Chip from '../Chip';
+import './style.css'
+
+class MiniCard extends PureComponent {
+
+    render() {
+        let { cardData } = this.props;
+        return (
+            cardData === null
+            ?   <div className="card">
+                    <div className="card-image">
+                        <div className="loading loading-lg"></div>
+                    </div>
+                </div>
+            :   <div className="card">
+                    <Link to={ `/detail/${ cardData.link }` }>
+                        <div className="card-image">
+                            <img className="img-responsive card-image-pm" src={ `https://www.pokemon.jp${ cardData.img }` } alt={ cardData.name } />
+                        </div>
+                        <div className="card-header">
+                            <div className="card-subtitle text-gray">No.{ cardData.zukan_no}</div>
+                            <div className="card-title h6">{ cardData.name }</div>
+                        </div>
+                        <div className="card-body">
+                            {
+                                cardData.type.split(',').map(type => <Chip type={ type } key={ type } />)
+                            }
+                        </div>
+                    </Link>
+                </div>
+        );
+    }
+}
+
+export default MiniCard;
--- client/src/views/Detail/index.js
+++ client/src/views/Detail/index.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Chip from '../../components/Chip';
+import MiniCard from '../../components/MiniCard';
 import { getDetailData } from '../../api';
+import './style.css';
 
 class Detail extends Component {
     state = {
@@ -15,10 +18,78 @@ class Detail extends Component {
         })
     }
     render() {
+        let { detailData } = this.state;
         return (
-            this.state.detailData === null
-            ?   <div className="content"><img src={ require('../../assets/images/loader.gif') } className="img-responsive loading" alt="loading" /></div>
-            :   <div>{ this.state.detailData.name }</div>
+            <div className="detail container">
+                {
+                    detailData === null
+                    ?   <div className="detail__content">
+                            <img src={ require('../../assets/images/loader.gif') } className="img-responsive loading" alt="loading" />
+                        </div>
+                    :   <div className="detail__content">
+                            <div className="detail__content-title card-list columns col-gapless">
+                                <div className="column col-1">
+                                    <i className="icon icon-2x icon-arrow-left"></i>
+                                </div>
+                                <div className="column col-10">
+                                    <h3 className="text-center"><span>{ detailData.num }</span>{ detailData.name }</h3>
+                                </div>
+                                <div className="column col-1">
+                                    <i className="icon icon-2x icon-arrow-right"></i>
+                                </div>
+                            </div>
+
+                            <div className="detail__content-intro card-list columns col-gapless">
+                                <div className="column col-6">
+                                    <img src={`https://www.pokemon.jp${detailData.profilePhoto}`} className="img-responsive" alt="profilePhoto"/>
+                                </div>
+                                <div className="column col-6">
+                                    <h5>タイプ</h5>
+                                    {
+                                        detailData.type.split(',').map(type => <Chip type={type} key={type} />)
+                                    }
+                                    <h5>弱点<span>※(★)はダメージ4倍</span></h5>
+                                    {
+                                        detailData.weaknesses.split(',').map(type => <Chip type={type} key={type} />)
+                                    }
+                                    <p>分類:{ detailData.details[0] }</p>
+                                    <p>特性:{ detailData.details[1] }</p>
+                                    <p>高さ:{ detailData.details[2] }</p>
+                                    <p>重さ:{ detailData.details[3] }</p>
+                                    <p>性別:{ detailData.details[4].split(',').map(sexy => <span className="chip" key={ sexy }> { sexy === 'sex-m' ? '♂' : '♀' }</span>) }</p>
+                                </div>
+                            </div>
+                            <div className="detail__content-form">
+                                <h5>すがた</h5>
+                                <div className="detail__appear">
+                                    {
+                                        detailData.pokemonForm.length === 0
+                                        ?   <h1>なし</h1>
+                                        :   detailData.pokemonForm.map((item, index) => <MiniCard cardData={ item } key={ index } />)
+                                    }
+                                </div>
+                                <h5>進 化</h5>
+                                <div className="detail__evolution">
+                                    {
+                                        detailData.evolution.length === 0
+                                        ?   <h1>なし</h1>
+                                        :   detailData.evolution.map((item, index) => <MiniCard cardData={ item } key={ index } />)
+                                    }
+                                </div>
+                                <h5>進 化 分 岐</h5>
+                                <div className="detail__evolution-branch">
+                                    {
+                                        detailData.evolution_branch.length === 0
+                                        ?   <h1>なし</h1>
+                                        :   detailData.evolution_branch.map((item, index) => <MiniCard cardData={ item } key={ index } />)
+                                    }
+                                </div>
+
+                            </div>
+                        </div>
+
+                }
+            </div>
         );
     }
 }
