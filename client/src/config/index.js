8d8f28c73991dacc0519b92bae4e451d6193ded1
grey0207 <grey0207@qq.com>
2018-09-24 16:08:47 +0800

    modify BASE_URL

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001'
+const BASE_URL = `${window.location.protocol}//${window.location.hostname}:3001`
 const ZUKAN_TOP = `${BASE_URL}/`
 const API = `${BASE_URL}/api`
 const DETAIL = `${BASE_URL}/detail`
